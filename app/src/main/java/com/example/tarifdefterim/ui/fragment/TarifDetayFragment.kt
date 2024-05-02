2daddda8c934b9ab99bf6bea81e63d6270e01e64
Furkan YAGLI <210542010@firat.edu.tr>
2024-05-02 19:28:47 +0300

    tarifDefterim

--- app/src/main/java/com/example/tarifdefterim/ui/fragment/TarifDetayFragment.kt
+++ app/src/main/java/com/example/tarifdefterim/ui/fragment/TarifDetayFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.navigation.fragment.navArgs
 import com.example.tarifdefterim.R
 import com.example.tarifdefterim.databinding.FragmentTarifDetayBinding
@@ -14,14 +15,15 @@ class TarifDetayFragment : Fragment() {
     private lateinit var binding: FragmentTarifDetayBinding
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        binding = FragmentTarifDetayBinding.inflate(inflater,container,false)
+        binding = DataBindingUtil.inflate(inflater,R.layout.fragment_tarif_detay, container,false)
+        binding.tarifDetayFragment = this
+
 
         val bundle: TarifDetayFragmentArgs by navArgs()
         val gelenTarif = bundle.tarif
 
-        binding.textAd.text = gelenTarif.yemek_ad
-        binding.textTarif.text = gelenTarif.yemek_tarif
-        binding.textMalzemeler.text = gelenTarif.yemek_malzemeler
+        binding.yemekNesnesi = gelenTarif
+
 
         return binding.root
     }
